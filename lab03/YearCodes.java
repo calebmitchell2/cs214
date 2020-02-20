2c09177cb118bd8c232b417c9564601988c2f3db
David Reidsma <david.reidsma@gmail.com>
2020-02-20 14:06:29 -0500

    finished lab 3 coding, need to make script files

---
+++ lab03/YearCodes.java
@@ -0,0 +1,32 @@
+/* YearCodes.java is a driver for function yearCode().
+ *
+ * Begun by: Prof. Adams, for CS 214 at Calvin College.
+ ****************************************************************/
+
+import java.util.Scanner;
+
+public class YearCodes {
+
+   public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("\nEnter your academic year: "); // prompt
+
+      String year = keyboard.next();      // read year
+      System.out.println(yearCode(year)); // display its code
+   }
+
+   /***************************************************************
+    * yearCode() converts an academic year into its integer code.
+    *
+    * Receive: year, a string.
+    * PRE: year is one of {freshman, sophomore, junior, or senior}.
+    * Return: the integer code corresponding to year.
+    */
+    public static int yearCode(String year) {
+        if (year.equalsIgnoreCase("freshman")) return 1;
+        if (year.equalsIgnoreCase("sophomore")) return 2;
+        if (year.equalsIgnoreCase("junior")) return 3;
+        if (year.equalsIgnoreCase("senior")) return 4;
+        return 0;
+    }
+}
